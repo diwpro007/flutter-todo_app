36d45ff21a6bdf19c125439bc07fe57d0fed9fb6
Diwakar Serala <diwakar.serala@pdc01.lisnepal.com.np>
2020-03-04 17:14:10 +0545

    Added user info

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -22,7 +23,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var appColors = [Color.fromRGBO(231, 129, 109, 1.0),Color.fromRGBO(99, 138, 223, 1.0),Color.fromRGBO(111, 194, 173, 1.0)];
+  var appColors = [
+    Color.fromRGBO(231, 129, 109, 1.0),
+    Color.fromRGBO(99, 138, 223, 1.0),
+    Color.fromRGBO(111, 194, 173, 1.0)
+  ];
   var cardIndex = 0;
 
   @override
@@ -45,7 +50,64 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0.0,
       ),
       drawer: Drawer(),
-      body: new Center(),
+      body: new Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 64.0, vertical: 32.0),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 45.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 12.0),
+                      child: Text(
+                        "Hello, Jane.",
+                        style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      ),
+                    ),
+                    Text(
+                      "Looks like feel good.",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "You have 3 tasks to do today",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                  child: Text(
+                    "TODAY : JUL 21, 2018",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  height: 300.0,
+                  color: Colors.white,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
       backgroundColor: appColors[cardIndex],
     );
   }
